2f5173326a78d02262263323743c6bd7ceab7f70
Mehmet Ali Ertorer <maertorer@gmail.com>
2025-02-07 16:35:16 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Mehmet Ali Ertorer on 7.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
